894eb25121e8c7eafc09f9a7f45e8f549af94ff5
Neel Upadhyaya <bahulneel@gmail.com>
2014-12-13 21:56:50 +0000

    updated version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject vivaldi "0.1.0-SNAPSHOT"
+(defproject vivaldi "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
